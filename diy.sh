7b2a1906c80b453779d96b5707dd7fa2bb569030
kavencats <shi772425803@163.com>
2026-07-11 20:41:47 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/TR3000/g' package/base-files/files/bin/config_generate
-if grep -q "luci-app-ssr-plus" .config; then
-  echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
-  ./scripts/feeds update helloworld
-  ./scripts/feeds install -a -p helloworld
-fi
